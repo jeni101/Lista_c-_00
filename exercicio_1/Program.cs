680df0359a9657c0dac16d0c6bfe8782ecee14ef
Jenifer gomes <jenifer.c.gomes@gmail.com>
2025-04-07 23:10:46 -0300

    Realizacao do exercicio 1, somatorio.

---
+++ exercicio_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SomatorioApp;
+
+namespace SomatorioApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Digiete ate que numero deseja somar: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = Somatoria.Somatorio(numero);
+            Console.Clear();
+            Console.WriteLine($"a somatoria final foi de: {resultado}");
+
+        }
+    }
+}
---
+++ exercicio_1/models/Somatoria.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SomatorioApp{
+
+
+    public class Somatoria
+    {
+        public static int Somatorio(int num)
+        {
+            return num * (num +1 ) /2;
+        }
+    }
+}
