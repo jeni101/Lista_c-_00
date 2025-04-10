c6f1c6b39f84c52b9630b0c77f2fc4904608c925
Jenifer gomes <jenifer.c.gomes@gmail.com>
2025-04-10 20:46:49 -0300

    resolvendo problemas

--- exercicio_1/obj/Debug/net9.0/exercicio_1.AssemblyInfo.cs
+++ exercicio_1/obj/Debug/net9.0/exercicio_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9e50254acefb5fe93a90c15736d9783fb504ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdcb03f981035cd5eb241cb3d5b002789eb98e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
